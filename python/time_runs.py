1bb6c17ca93bcd5a1db8f98e9071e1cd14658339
Artur Hallmann <a.hallmann@neusta.de>
2015-02-01 23:46:00 +0100

    .

--- python/time_runs.py
+++ python/time_runs.py
@@ -59,7 +59,57 @@ problems = [
     "Distinct primes factors",
     "Self powers",
     "Prime permutations",
-    "Consecutive prime sum"
+    "Consecutive prime sum",
+    "Prime digit replacements",
+    "Permuted multiples",
+    "Combinatoric selections",
+    "Poker hands",
+    "Lychrel numbers",
+    "Powerful digit sum",
+    "Square root convergents",
+    "Spiral primes",
+    "XOR decryption",
+    "Prime pair sets",
+    "Cyclical figurate numbers",
+    "Cubic permutations",
+    "Powerful digit counts",
+    "Odd period square roots",
+    "Convergents of e",
+    "Diophantine equation",
+    "Maximum path sum II",
+    "Magic 5-gon ring",
+    "Totient maximum",
+    "Totient permutation",
+    "Ordered fractions",
+    "Counting fractions",
+    "Counting fractions in a range",
+    "Digit factorial chains",
+    "Singular integer right triangles",
+    "Counting summations",
+    "Prime summations",
+    "Coin partitions",
+    "Passcode derivation",
+    "Square root digital expansion",
+    "Path sum: two ways",
+    "Path sum: three ways",
+    "Path sum: four ways",
+    "Monopoly odds",
+    "Counting rectangles",
+    "Cuboid route",
+    "Prime power triples",
+    "Product-sum numbers",
+    "Roman numerals",
+    "Cube digit pairs",
+    "Right triangles with integer coordinates",
+    "Square digit chains",
+    "Arithmetic expressions",
+    "Almost equilateral triangles",
+    "Amicable chains",
+    "Su Doku",
+    "Large non-Mersenne prime",
+    "Anagramic squares",
+    "Largest exponential",
+    "Arranged probability"
 ]
 
 
@@ -73,17 +123,16 @@ while line:
     line = f_tmpl.readline()
 f.write("\n")
 
-def makeline(nr, name, result, solution, link1, link2):
+def makeline(nr, name, result, link1, link2):
     return "| " + str(nr).rjust(4) + " | " + name.rjust(40) + " | " + result.rjust(10) + " | " + \
-           solution.rjust(20) + " | " + link1.rjust(60) + " | " + link2.rjust(100) + " |\n"
+          link1.rjust(60) + " | " + link2.rjust(100) + " |\n"
 
-f.write(makeline("#", "Name", "Seconds", "Spoiler!", "", ""))
-f.write(makeline("-" * 4, "-" * 40, "-" * 10, "-" * 20, "-" * 60, "-" * 100))
+f.write(makeline("#", "Name", "Seconds", "", ""))
+f.write(makeline("-" * 4, "-" * 40, "-" * 10, "-" * 60, "-" * 100))
 
-for o in range(1, 51):
-    solution = None
+for o in range(1, 101):
     try:
-        t = timeit.Timer("solution = run()", "from src.problem%03d" % o + " import run")
+        t = timeit.Timer("run()", "from src.problem%03d" % o + " import run")
         seconds = t.timeit(1)
         # seconds = 3.3
         result = str(round(seconds, 4)).ljust(6, '0')
@@ -96,10 +145,11 @@ for o in range(1, 51):
     name = problems[o-1]
     link1 = "[Problem](https://projecteuler.net/problem=%d)" % o
     link2 = "[Solution](https://github.com/arturh85/projecteuler/blob/master/python/src/problem%03d.py)" % o
-    
-    line = makeline(o, name, result, ">! " + str(solution), link1, link2)
+    if not os.path.isfile("problem%03d.py" % o):
+        link2 = ""
+    line = makeline(o, name, result, link1, link2)
     f.write(line)
-    print o, name, solution, result
+    print o, name, result
 
 f_tmpl.close()
 f.close()
