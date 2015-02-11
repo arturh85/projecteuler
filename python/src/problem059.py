ab38db6db5b9b2e488ef5b58431e724a021c77df
Artur Hallmann <a.hallmann@neusta.de>
2015-02-11 13:30:05 +0100

    problem89

--- python/src/problem059.py
+++ python/src/problem059.py
@@ -1,63 +1,72 @@
 # coding=utf-8
-'''
+"""
 Problem 59
 12 September 2013
 
-Each character on a computer is assigned a unique code and the preferred standard is ASCII (American Standard Code for Information Interchange). For example, uppercase A = 65, asterisk (*) = 42, and lowercase k = 107.
+Each character on a computer is assigned a unique code and the preferred standard is ASCII 
+(American Standard Code for Information Interchange). For example, uppercase A = 65, asterisk (*) = 42, 
+and lowercase k = 107.
 
-A modern encryption method is to take a text file, convert the bytes to ASCII, then XOR each byte with a given value, taken from a secret key. The advantage with the XOR function is that using the same encryption key on the cipher text, restores the plain text; for example, 65 XOR 42 = 107, then 107 XOR 42 = 65.
+A modern encryption method is to take a text file, convert the bytes to ASCII, then XOR each byte with a given value, 
+taken from a secret key. The advantage with the XOR function is that using the same encryption key on the cipher text, 
+restores the plain text; for example, 65 XOR 42 = 107, then 107 XOR 42 = 65.
 
-For unbreakable encryption, the key is the same length as the plain text message, and the key is made up of random bytes. The user would keep the encrypted message and the encryption key in different locations, and without both "halves", it is impossible to decrypt the message.
+For unbreakable encryption, the key is the same length as the plain text message, and the key is made up of random 
+bytes. The user would keep the encrypted message and the encryption key in different locations, and without both 
+"halves", it is impossible to decrypt the message.
 
-Unfortunately, this method is impractical for most users, so the modified method is to use a password as a key. If the password is shorter than the message, which is likely, the key is repeated cyclically throughout the message. The balance for this method is using a sufficiently long password key for security, but short enough to be memorable.
+Unfortunately, this method is impractical for most users, so the modified method is to use a password as a key. If 
+the password is shorter than the message, which is likely, the key is repeated cyclically throughout the message. 
+The balance for this method is using a sufficiently long password key for security, but short enough to be memorable.
 
-Your task has been made easy, as the encryption key consists of three lower case characters. Using cipher.txt (right click and 'Save Link/Target As...'), a file containing the encrypted ASCII codes, and the knowledge that the plain text must contain common English words, decrypt the message and find the sum of the ASCII values in the original text.
+Your task has been made easy, as the encryption key consists of three lower case characters. Using cipher.txt (right 
+click and 'Save Link/Target As...'), a file containing the encrypted ASCII codes, and the knowledge that the plain 
+text must contain common English words, decrypt the message and find the sum of the ASCII values in the original text.
 ----------------------------------------------------------
 Created on 10.02.2015
 
 @author: ahallmann
-'''
+"""
 import unittest
-import collections
 import timeit
 import string
 
 
-def encrypt(text, cipher):
-    s = []
+def encrypt(plain_text, password):
+    cipher_text_values = []
     
-    for i in range(len(text)):
-        c = text[i]
-        p = cipher[i % len(cipher)]
-        
-        s.append(str(ord(c) ^ ord(p)))
+    for i in range(len(plain_text)):
+        ascii = ord(plain_text[i])
+        p = ord(password[i % len(password)])
+        cipher_text_values.append(str(ascii ^ p))
 
-    return ','.join(s)
+    return ','.join(cipher_text_values)
+
     
 def decrypt(text, cipher):
     s = text.split(',')
     plaintext = ''
     for i in range(len(s)):
         c = int(s[i])
-        p = cipher[i % len(cipher)]
+        p = ord(cipher[i % len(cipher)])
         
-        plaintext += chr(c ^ ord(p))
+        plaintext += chr(c ^ p)
     
     return plaintext
     
 
-def read_cipher(filename):
+def read_cipher_text(filename):
     f = open(filename, 'r')
-    hands = []
     line = f.readline()
     f.close()
     return line
 
-def correct(text):
+
+def is_plausible_solution(plain_text):
     permitted = string.ascii_letters + string.whitespace + string.digits + '.!;\',()'
     
-    for c in text:
-        if c not in permitted:
+    for char in plain_text:
+        if char not in permitted:
             return False
     return True
     
@@ -66,24 +75,26 @@ def char_range(c1, c2):
     for c in xrange(ord(c1), ord(c2)+1):
         yield chr(c)
     
+    
 def solve():
-    cipher = read_cipher("data/problem059.txt")
-
+    cipher_text = read_cipher_text("data/problem059.txt")
     for a in char_range('a', 'z'):
         for b in char_range('a', 'z'):
             for c in char_range('a', 'z'):
-                x = decrypt(cipher, a + b + c)
-                if correct(x):
-                    return sum(map(ord, list(x)))
-    return -1
+                password = a + b + c
+                plain_text = decrypt(cipher_text, password)
+                if is_plausible_solution(plain_text):
+                    return sum(map(ord, list(plain_text)))
+    return None
+
 
 class Test(unittest.TestCase):
     def test_sample(self):
         self.assertEqual("7,14,14", encrypt("foo", "a"))
         self.assertEqual("foo", decrypt("7,14,14", "a"))
-        self.assertTrue(correct("foo"))
-        self.assertFalse(correct("föö"))
-        self.assertFalse(correct("foo{}"))
+        self.assertTrue(is_plausible_solution("foo bar!"))
+        self.assertFalse(is_plausible_solution("fï¿½ï¿½"))
+        self.assertFalse(is_plausible_solution("foo{}"))
         
         pass
     
---
+++ python/src/problem089.py
@@ -0,0 +1,183 @@
+# coding=utf-8
+"""
+Problem 89
+18 February 2005
+
+For a number written in Roman numerals to be considered valid there are basic rules which must be followed. 
+Even though the rules allow some numbers to be expressed in more than one way there is always a "best" way 
+of writing a particular number.
+
+For example, it would appear that there are at least six ways of writing the number sixteen:
+
+IIIIIIIIIIIIIIII
+VIIIIIIIIIII
+VVIIIIII
+XIIIIII
+VVVI
+XVI
+
+However, according to the rules only XIIIIII and XVI are valid, and the last example is considered to be the most 
+efficient, as it uses the least number of numerals.
+
+The 11K text file, roman.txt (right click and 'Save Link/Target As...'), contains one thousand numbers written in 
+valid, but not necessarily minimal, Roman numerals; see About... Roman Numerals for the definitive rules for 
+this problem.
+
+Find the number of characters saved by writing each of these in their minimal form.
+
+Note: You can assume that all the Roman numerals in the file contain no more than four consecutive identical units.
+
+----------------------------------------------------------
+Created on 10.02.2015
+
+@author: ahallmann
+"""
+import unittest
+import timeit
+
+
+def read_numerals(filename):
+    f = open(filename, 'r')
+    numerals = []
+    for line in f.readlines():
+        numerals.append(line.strip())
+
+    f.close()
+    return numerals
+
+
+def format_roman_numeral(number):
+    s = ''
+    if 900 <= number < 1000:
+        s += 'C'
+        number += 100
+    while number >= 1000:
+        s += 'M'
+        number -= 1000
+        if 900 <= number < 1000:
+            s += 'C'
+            number += 100
+    if 400 <= number < 500:
+        s += 'C'
+        number += 100
+    if number >= 500:
+        s += 'D'
+        number -= 500
+    if 90 <= number < 100:
+        s += 'X'
+        number += 10
+    while number >= 100:
+        s += 'C'
+        number -= 100
+        if 90 <= number < 100:
+            s += 'X'
+            number += 10
+    if 40 <= number < 50:
+        s += 'X'
+        number += 10
+    if number >= 50:
+        s += 'L'
+        number -= 50
+    if number == 9:
+        s += 'I'
+        number += 1
+    while number >= 10:
+        s += 'X'
+        number -= 10
+        if number == 9:
+            s += 'I'
+            number += 1
+    if number == 4:
+        s += 'I'
+        number += 1
+    if number >= 5:
+        s += 'V'
+        number -= 5
+    while number >= 1:
+        s += 'I'
+        number -= 1
+    return s
+
+
+def parse_roman_numeral(numeral):
+    mapping = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    order = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+    last_order = -1
+
+    s = 0
+    for c in numeral:
+        if c not in mapping: 
+            raise ValueError("unknown char: " + c)
+        
+        current_order = order.index(c)
+        if current_order < last_order:
+            s -= mapping[order[last_order]] * 2
+        
+        last_order = current_order
+        
+        s += mapping[c]
+
+    return s
+
+
+def optimize_roman_numeral(numeral):
+    return format_roman_numeral(parse_roman_numeral(numeral))
+
+
+def solve():
+    numerals = read_numerals("data/problem089.txt")
+    savings = 0
+    for numeral in numerals:
+        o = optimize_roman_numeral(numeral)
+        savings += len(numeral) - len(o)
+    return savings
+
+
+class Test(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(16, parse_roman_numeral('XVI'))
+        self.assertEqual('XVI', format_roman_numeral(16))
+        self.assertEqual(4, parse_roman_numeral('IV'))
+        self.assertEqual('IV', format_roman_numeral(4))
+        self.assertEqual(19, parse_roman_numeral('XIX'))
+        self.assertEqual('XIX', format_roman_numeral(19))
+        self.assertEqual(40, parse_roman_numeral('XL'))
+        self.assertEqual('XL', format_roman_numeral(40))
+        self.assertEqual(90, parse_roman_numeral('XC'))
+        self.assertEqual('XC', format_roman_numeral(90))
+        self.assertEqual(400, parse_roman_numeral('CD'))
+        self.assertEqual('CD', format_roman_numeral(400))
+        self.assertEqual(900, parse_roman_numeral('CM'))
+        self.assertEqual('CM', format_roman_numeral(900))
+        self.assertEqual('MMMMDXCV', format_roman_numeral(4595))
+        self.assertEqual('XCV', format_roman_numeral(95))
+        pass
+    
+    def test_answer(self):
+        self.assertEqual(743, solve())
+        pass
+
+
+# -----------------------------------------
+
+
+def run():
+    return solve()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# if __name__ == '__main__':
+# t = timeit.Timer("run()", "from __main__ import run")
+# count = 1
+# print(str(t.timeit(count)) + " seconds for " + str(count) + " runs")
